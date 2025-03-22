042f875e07947755f519dc6603e32b281909b24c
Nurio Fernández <n.fernandez@efg.gg>
2025-03-22 16:19:23 +0100

    Included environment variable in the deployment as a secret for testing

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("Starting K3s Glaucus!")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
-		fmt.Fprintf(w, "K3S on Glaucus! With CI/CD! Fancy!")
+		fmt.Fprintf(w, "K3S on Glaucus! With CI/CD! Fancy! "+os.Getenv("SECRET"))
 	})
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
